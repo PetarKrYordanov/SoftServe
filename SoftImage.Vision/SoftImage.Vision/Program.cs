231abca1bc76128b59661b2b6877f003ad4c6f4f
Petar Yordanov <pesho_kr@abv.bg>
2019-02-20 11:52:01 +0200

    simple refactoring

--- SoftImage.Vision/SoftImage.Vision/Program.cs
+++ SoftImage.Vision/SoftImage.Vision/Program.cs
@@ -9,16 +9,16 @@ namespace SoftImage.Vision
     {
         public static void Main(string[] args)
         {
-           // Console.WriteLine("Enter picture name");
-           //var imageName =  Console.ReadLine();
+            Console.WriteLine("Enter picture name");
+            var imageName = Console.ReadLine();
 
             BlobService blobService = new BlobService();
-            string url = blobService.GetPictureUrlByName("wall2");
+            string url = blobService.GetPictureUrlByName(imageName);
 
             IImageInformation imageInformation = new TagInformation();
             var tagsInfo = imageInformation.GetAnalysisAsync(url);
 
-            Console.WriteLine("Computer vision found tags:");
+            Console.WriteLine("Computer vision found the following tags:");
             foreach (var item in tagsInfo.Tags)
             {
                 Console.WriteLine($"{item.Name} with confidence {item.Confidence:f3}");
